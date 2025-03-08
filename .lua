c59c5217de15d1f8197a53b1dc385d1bfd436ec1
vipqua123ok <nnsads005@gmail.com>
2025-03-08 14:08:53 +0700

    Update and rename README.md to .lua

---
+++ .lua
@@ -0,0 +1,5 @@
+  JoinTeam = "Marines"; -- Pirates/Marines
+  Translator = true; -- true/false
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/realredz/BloxFruits/refs/heads/main/Source.lua"))
